10297bc0025ab6ea824e385312f2ed197ff1bfbe
Daniel Boham <hello@danielboham.com>
2020-11-16 21:36:36 +0100

    Created 2 methods in Game class

--- js/Game.js
+++ js/Game.js
@@ -5,7 +5,20 @@
 class Game {
     constructor() {
         this.missed = 0;
-        this.phrases = [];
+        this.phrases = this.createPhrases();
         this.activePhrase = null;
+    }
+    createPhrases() {
+        const array = [
+            { phrase: "this is phrase 1"},
+            { phrase: "this is phrase 2"},
+            { phrase: "this is phrase 3"},
+            { phrase: "this is phrase 4"},
+            { phrase: "this is phrase 5"}
+        ]
+        return array;
+    }
+    getRandomPhrase(){
+        return this.phrases[Math.floor(Math.random()*this.phrases.length)];
     }
 }
--- js/app.js
+++ js/app.js
@@ -5,3 +5,20 @@
 // Test - const phrase = new Phrase("Hello Does This Work?");
 // console.log(`${phrase.phrase}`);
 
+/* Test - const game = new Game();
+game.phrases.forEach((phrase, index) => {
+    console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+});
+*/
+
+/* Test -
+const logPhrase = (phrase) => {
+    console.log(`Phrase - phrase: `, phrase.phrase);
+};
+const game = new Game();
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+*/
