e2253b027a75d37f8c2ea1beddb4ec2ddcf43944
Daniel Boham <hello@danielboham.com>
2020-11-23 23:00:21 +0100

    Finished removeLife() method

--- js/Game.js
+++ js/Game.js
@@ -19,10 +19,10 @@ class Game {
         ]
         return array;
     }
-    getRandomPhrase(){
+    getRandomPhrase() {
         return this.phrases[Math.floor(Math.random()*this.phrases.length)];
     }
-    startGame(){
+    startGame() {
         //Target div with overlay id and hide by setting style on display - none.
         const overlay = document.getElementById('overlay');
         overlay.style.display = "none";
@@ -37,13 +37,34 @@ class Game {
         //this.activePhrase.showMatchedLetter();
 
     }
-    checkForWin(){
+    checkForWin() {
+        // Get all elements with class hide..
         let win = document.getElementsByClassName("hide ");
+        console.log(win);
+        // Conditional if the length of the HTML collection is > 0
         if(win.length > 0) {
             return false;
         } else {
             return true;
         }
+    }
+    removeLife() {
+        // By calling this method increase this.missed by 1
+        this.missed += 1;
+        // Target the first element with img liveHeart
+        let lives = document.querySelector('img[src="images/liveHeart.png"]');
+        // Condition for missed attempts change first found element to lostHeart
+        if (this.missed < 5) {
+            lives.src = "images/lostHeart.png";
+        } else if (this.missed === 5) {
+            console.log("this.gameOver();");
+        }
+
+
+        console.log(this.missed);
+    }
+    gameOver() {
+
     }
 }
 
